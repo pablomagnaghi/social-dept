a2f0da12b1a9ab2ff0a1d37d0eccfca77f7aa25c
Pablo Magnaghi <pablomagnaghi@gmail.com>
2025-07-17 01:09:09 -0300

    init project with Next.js 15 and clerk

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default async function DashboardPage() {
+  return (
+    <div className="max-w-screen-xl mx-auto py-5">
+      <h1 className="text-4xl font-semibold pb-5">Dashboard</h1>
+    </div>
+  );
+}
